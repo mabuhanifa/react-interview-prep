744f00c74cfc0c6888070289919ad1879121e75e
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-05 23:55:06 +0600

    useEffect

--- src/components/Hooks.js
+++ src/components/Hooks.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Hooks() {
   const [state, setState] = useState({
     name: "hooks",
     type: "react.setState",
   });
+  useEffect(()=>{
+
+  },[])
   return (
     <div>
       <h1>Hooks</h1>
+      <h2>useState</h2>
       <div>
         <h2>{state.name}</h2>
         <button
@@ -15,6 +19,10 @@ export default function Hooks() {
         >
           Change
         </button>
+      </div>
+      <h2>useEffect</h2>
+      <div>
+
       </div>
     </div>
   );
