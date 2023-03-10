e16e86f63a8b2cb7cdfd5604876ef5550fc71d60
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-10 17:41:18 +0600

    context created

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,8 +1,17 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useReducer } from "react";
 
-export default function Context() {
-    const Provider = createContext();
-  return (
-    <div>Context</div>
-  )
+const initialState = {};
+
+const reducer = (state, action) => {};
+
+const Provider = createContext();
+
+export default function Context({ children }) {
+  const value = useReducer(initialState, reducer);
+
+  return <Provider.Provider value={value}>{children}</Provider.Provider>;
 }
+
+export const useSelect = () => {
+  return useContext(Provider);
+};
