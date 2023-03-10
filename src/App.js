b366f95ed6c134ea0ff207a18dab1e5df87ad4a9
shourovahmd <moh.abuhanifa@gmail.com>
2023-03-10 18:14:26 +0600

    context implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Hooks from "./components/Hooks";
+import { useSelect } from "./context/Context";
 
 function App() {
+  const [state, dispatch ] = useSelect();
+  console.log(state.value);
   return (
     <div>
       <div>
-        <Hooks/>
+        <Hooks />
       </div>
     </div>
   );
--- src/context/Context.js
+++ src/context/Context.js
@@ -1,15 +1,21 @@
 import React, { createContext, useContext, useReducer } from "react";
 
-const initialState = {};
+const initialState = {
+  value: 0,
+};
 
 const reducer = (state, action) => {};
 
 const Provider = createContext();
 
 export default function Context({ children }) {
-  const value = useReducer(initialState, reducer);
+  const value = useReducer(reducer, initialState);
 
-  return <Provider.Provider value={value}>{children}</Provider.Provider>;
+  return (
+    <Provider.Provider value={value}>
+      {children}
+    </Provider.Provider>
+  );
 }
 
 export const useSelect = () => {
